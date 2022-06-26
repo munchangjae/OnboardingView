d71f7dd325a276729d6c5e88a650ee0e23b9a90e
munchangjae <107340769+munchangjae@users.noreply.github.com>
2022-06-26 21:17:43 +0900

    Add files via upload

---
+++ PhotoGalleryApp/PhotoGalleryApp/photoCell.swift
@@ -0,0 +1,13 @@
+//
+//  photoCell.swift
+//  PhotoGalleryApp
+//
+//  Created by 문창재 on 2022/06/09.
+//
+
+import UIKit
+
+class photoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+}
